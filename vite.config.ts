9140444e0af9fbfc6036d7a44ec9f2ad8e784c02
Wilson Gramer <12683684+WilsonGramer@users.noreply.github.com>
2026-08-20 08:20:47 -0400

    Configure CORS in dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 /// <reference types="vitest/config" />
 
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, type Plugin, type PreviewServer, type ViteDevServer } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import wasm from "vite-plugin-wasm";
 import tailwindcss from "@tailwindcss/vite";
 import yaml from "@modyfi/vite-plugin-yaml";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [svelte(), wasm(), tailwindcss(), yaml()],
+export default defineConfig(() => ({
+    plugins: [svelte(), wasm(), tailwindcss(), yaml(), crossOriginIsolation()],
     resolve: {
         alias: {
             "@": path.resolve("./src"),
@@ -19,4 +19,18 @@ export default defineConfig({
         resolveSnapshotPath: (testPath, snapExtension) => testPath + snapExtension,
         passWithNoTests: true,
     },
+}));
+
+const configureServer = (server: ViteDevServer | PreviewServer) => {
+    server.middlewares.use((_req, res, next) => {
+        res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
+        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+        next();
+    });
+};
+
+const crossOriginIsolation = (): Plugin => ({
+    name: "cross-origin-isolation",
+    configureServer,
+    configurePreviewServer: configureServer,
 });
